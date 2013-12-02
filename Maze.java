a177185ef0202016d61fe2c30a101128944144a7
ademirf <ademirf.poa@gmail.com>
2013-12-02 13:06:18 -0200

    Create Maze.java

---
+++ Maze.java
@@ -0,0 +1,78 @@
+
+public class Global {
+
+	// VARIÁVEIS DO JOGADOR
+	public static boolean isLive;
+	public static boolean isWinner;
+	public static int previousPosition;
+	public static int playerPosition;
+	public static String playerName;
+	public static int playerLife;
+	public static int playerAttackmin;
+	public static int playerAttackmax;
+	public static int playerResistancemin;
+	public static int playerResistancemax;
+	public static String playerWeapon;
+	public static String playerArmor;
+	public static String playerKey;
+	public static int eliminations;
+	public static int playerEnergy;
+	public static int damage;
+	
+	// VARIÁVEIS DOS INIMIGOS
+	public static String enemyName;
+	public static int enemyLife;
+	public static int enemyAttackmin;
+	public static int enemyAttackmax;
+	public static int enemyResistancemin;
+	public static int enemyResistancemax;
+	
+	// VARIÁVEIS DE DEFINIÇÃO DO JOGADOR E DOS INIMIGOS
+	public static int optionPlayer;
+	public static String enemy1;
+	public static String enemy2;
+	public static String enemy3;
+	
+	// VARIÁVEIS DO CENÁRIO ATUAL
+	public static String nameRoomCurrent;
+	public static String roomCurrent;
+	public static String northCurrent;
+	public static String southCurrent;
+	public static String eastCurrent;
+	public static String westCurrent;
+	public static String upCurrent;
+	public static String downCurrent;
+	public static int sideNcurrent;
+	public static int sideScurrent;
+	public static int sideEcurrent;
+	public static int sideWcurrent;
+	public static int sideUcurrent;
+	public static int sideDcurrent;
+	public static String weaponRoomCurrent;
+	public static String armorRoomCurrent;
+	public static String keyRoomCurrent;
+	public static String obstacleCurrent;
+	
+	// VARIÁVEIS CLASSE RANDOM
+	public static int minimum;
+	public static int maximum;
+	public static int range;
+	public static double numberRandom;
+	public static int blow;
+	public static int defense;
+	
+	// VARIÁVEIS ARMADILHA
+	public static int right;
+	public static int left;
+	public static int center;
+	
+	// VARIÁVEIS ITENS
+	public static int number;
+	public static int counter;
+	public static String enemy;
+	public static String key;
+	public static String weapon;
+	public static String armor;
+	public static String door;
+
+}
